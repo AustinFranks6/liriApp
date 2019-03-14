1d9d69a795b1fceaf7046aff0e08aed7a1b9368f
Franks40e <austinfranks704@gmail.com>
2019-03-13 21:09:58 -0400

    update

--- liri.js
+++ liri.js
@@ -13,41 +13,8 @@ spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(er
     console.log(data);
 });
 
-var getMeSpotify = function(songName) {
-    if (songName === undefined) {
-      songName = "What's my age again";
-    }
-  
-    spotify.search(
-      {
-        type: "track",
-        query: songName
-      },
-      function(err, data) {
-        if (err) {
-          console.log("Error occurred: " + err);
-          return;
-        }
-  
-        var songs = data.tracks.items;
-  
-        for (var i = 0; i < songs.length; i++) {
-          console.log(i);
-          console.log("artist(s): " + songs[i].artists.map(getArtistNames));
-          console.log("song name: " + songs[i].name);
-          console.log("preview song: " + songs[i].preview_url);
-          console.log("album: " + songs[i].album.name);
-          console.log("-----------------------------------");
-        }
-      }
-    );
-  };
-  
 var pick = function(caseData, functionData) {
     switch (caseData) {
-        case "spotify-this-song":
-        getMeSpotify(functionData);
-        break;
 
     default:
       console.log("LIRI doesn't know that");
@@ -59,3 +26,5 @@ var runThis = function(argOne, argTwo) {
 };
 
 runThis(process.argv[2], process.argv[3]);
+
+//woudl need to let liri app actually show something unsure of why it isnt
