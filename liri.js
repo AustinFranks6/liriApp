2e5a9726870d5fdd48b705bfd37c2736c323adc0
Franks40e <austinfranks704@gmail.com>
2019-03-13 21:05:22 -0400

    update

--- liri.js
+++ liri.js
@@ -1,13 +1,61 @@
 require("dotenv").config();
 
-let Spotify = require("node-spotify-api");
+let keys = require(".keys");
 
-let keys = require("./keys");
+var spotify = require('spotify'); 
+ 
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    console.log(data);
+});
 
-let axios = require("axios");
+var getMeSpotify = function(songName) {
+    if (songName === undefined) {
+      songName = "What's my age again";
+    }
+  
+    spotify.search(
+      {
+        type: "track",
+        query: songName
+      },
+      function(err, data) {
+        if (err) {
+          console.log("Error occurred: " + err);
+          return;
+        }
+  
+        var songs = data.tracks.items;
+  
+        for (var i = 0; i < songs.length; i++) {
+          console.log(i);
+          console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+          console.log("song name: " + songs[i].name);
+          console.log("preview song: " + songs[i].preview_url);
+          console.log("album: " + songs[i].album.name);
+          console.log("-----------------------------------");
+        }
+      }
+    );
+  };
+  
+var pick = function(caseData, functionData) {
+    switch (caseData) {
+        case "spotify-this-song":
+        getMeSpotify(functionData);
+        break;
 
-let moment = require("moment");
+    default:
+      console.log("LIRI doesn't know that");
+    }
+  };
 
-let fs = require("fs");
+var runThis = function(argOne, argTwo) {
+    pick(argOne, argTwo);
+};
 
-let spotify = new Spotify(keys.spotify);
+runThis(process.argv[2], process.argv[3]);
